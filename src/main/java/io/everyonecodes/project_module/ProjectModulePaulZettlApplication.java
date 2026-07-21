e2181aacf19347c3cdd7546364ad5aed61314654
Paul Zettl <paulhzettl@gmail.com>
2026-07-21 10:19:53 +0200

    initial commit

---
+++ src/main/java/io/everyonecodes/project_module/ProjectModulePaulZettlApplication.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.project_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModulePaulZettlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectModulePaulZettlApplication.class, args);
+    }
+
+}
